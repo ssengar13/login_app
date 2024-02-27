b9be4731aa7d560c0f72eb0731b1a626e4de39ae
Sonal Sengar <sengarsonal13@gmail.com>
2024-02-27 17:07:24 +0530

    added username module

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-function App() {
+/** import all components */
+import Username from './components/Username';
+import Password from './components/Password';
+import Profile from './components/Profile';
+import Recovery from './components/Recovery';
+import PageNotFound from './components/PageNotFound';
+import Reset from './components/Reset';
+import Register from './components/Register';
+
+
+/** root routes */
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Username></Username>
+  },
+  {
+    path: '/register',
+    element: <Register></Register>
+  },
+  {
+    path: '/password',
+    element: <Password></Password>
+  },
+  {
+    path: '/profile',
+    element: <Profile></Profile>
+  },
+  {
+    path: '/recovery',
+    element: <Recovery></Recovery>
+  },
+  {
+    path: '/reset',
+    element: <Reset></Reset>
+  },
+  {
+    path: '*',
+    element: <PageNotFound></PageNotFound>
+  },
+])
+
+export default function App() {
   return (
-    <div className="App">
-      <h1>React App</h1>
-    </div>
-  );
-}
+    <main>
+      <RouterProvider router={router}>
 
-export default App;
+      </RouterProvider>
+    </main>
+  )
+}
