8c16592845827c35161bc24ed51b6a029c778914
Sonal Sengar <sengarsonal13@gmail.com>
2024-02-29 17:53:22 +0530

    added register  module

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import avatar from '../assets/profile.png';
 import styles from '../styles/Username.module.css';
 import { Toaster } from 'react-hot-toast';
 import { useFormik } from 'formik';
-import { passwordValidate } from '../helper/validate.js'
+import { registerValidation } from '../helper/validate.js';
+import convertToBase64 from '../helper/convert.js';
 
 export default function Register() {
+
+  const[file, setFile] = useState();
 
   const formik = useFormik({
     initialValues: {
@@ -14,13 +17,21 @@ export default function Register() {
       username: 'example123',
       password: 'admin@123'
     },
-    validate: passwordValidate,
+    validate: registerValidation,
     validateOnChange: false,
     validateOnBlur: false,
     onSubmit: async values => {
+      values = await Object.assign(values, { profile : file || ''})
       console.log(values)
     }
   })
+
+/** formik doesn't support file upload so we need to create this handler */
+   const onUpload = async e => {
+    const base64 = await convertToBase64(e.target.files[0]);
+    setFile(base64);
+   }
+  
   return (
 
 <div className="container mx-auto">
@@ -28,27 +39,32 @@ export default function Register() {
 <Toaster position='top-center' reverseOrder={false}></Toaster>
 
 <div className='flex justify-center items-center h-screen'>
-  <div className={styles.glass}>
+  <div className={styles.glass} style={{height: "95%", width: "35%"}}>
 
     <div className="title flex flex-col items-center">
-      <h4 className='text-4xl font-bold'>Hello Again!</h4>
+      <h4 className='text-3xl font-bold'>Register</h4>
       <span className='py-4 text-md w-2/3 text-center text-gray-500'>
-        Explore More by connecting with us.
+        Happy to join you...!
       </span>
     </div>
 
     <form className='py-1' onSubmit={formik.handleSubmit}>
         <div className='profile flex justify-center py-4'>
-            <img src={avatar} className={styles.profile_img} alt="avatar" />
+          <label htmlFor='profile'>
+          <img src={file || avatar} className={styles.profile_img} alt="avatar" />
+          </label>
+          <input onChange={onUpload} type="file" id='profile' name='profile'/>
         </div>
 
         <div className="textbox flex flex-col items-center gap-6">
-            <input {...formik.getFieldProps('password')} className={styles.textbox} type="text" placeholder='Password' />
-            <button className={styles.btn} type='submit'>Sign In</button>
+            <input {...formik.getFieldProps('email')} className={styles.textbox} type="text" placeholder='Email*' />
+            <input {...formik.getFieldProps('username')} className={styles.textbox} type="text" placeholder='Username*' />
+            <input {...formik.getFieldProps('password')} className={styles.textbox} type="text" placeholder='Password*' />
+            <button className={styles.btn} type='submit'>Register</button>
         </div>
 
         <div className="text-center py-4">
-          <span className='text-gray-500 text-sm'>Forgot Password?<Link className='text-red-500 text-sm' to="/recovery"> Recover Now</Link></span>
+          <span className='text-gray-500 text-sm'>Already Register? <Link className='text-red-500 text-sm' to="/"> Login Now</Link></span>
         </div>
 
     </form>
--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -22,6 +22,15 @@ export async function resetPasswordValidation(values){
     return errors;
 }
 
+/** Validate Register Form */
+export async function registerValidation(values){
+    const errors = usernameVerify({}, values);
+    passwordVerify(errors, values);
+    emailVerify(errors, values);
+
+    return errors;
+}
+
 /** validate password */
 function passwordVerify(errors = {}, values){
     const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
@@ -47,5 +56,18 @@ function usernameVerify (error = {}, values){
     }else if(values.username.includes(" ")){
         error.username = toast.error('Invalid Username...!');
     }
+    return error;
+}
+
+/** validate email */
+function emailVerify(error ={}, values){
+    if(!values.email){
+        error.email = toast.error("Email Required...!");
+    }else if(values.email.includes(" ")){
+        error.email = toast.error("Wrong Email...!")
+    }else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
+        error.email = toast.error("Invalid email address...!")
+    }
+
     return error;
 }
