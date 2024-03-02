7cde44249a8cb6fffef8d7fd311d636d665f74f9
Sonal Sengar <sengarsonal13@gmail.com>
2024-03-02 12:38:04 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -1,5 +1,19 @@
 // const express = require ('express');
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
 
 const app = express();
+
+/** Middlewares */
+app.use(express.json());
+app.use(cors());
+app.use(morgan('tiny'));
+app.disable('x-powered-by'); //less hackers know about our stack
+
+const port = 8080;
+
+/** HTTP Get Request */
+app.get('/', (req, res) => {
+    res.status(201).json("Home GET Request");
+})
