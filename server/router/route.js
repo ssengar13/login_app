2eda1e9b7d8ba31fb88e16d85ca757eb8bf8f5a3
Sonal Sengar <sengarsonal13@gmail.com>
2024-03-05 18:44:33 +0530

    commit

---
+++ server/router/route.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+const router = Router();
+
+/** POST Methods */
+
+/** GET Methods */
+
+/** Put Methods */
+
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import connect from './database/conn.js';
+import router from './router/route.js';
 
 const app = express();
 
@@ -17,3 +19,19 @@ const port = 8080;
 app.get('/', (req, res) => {
     res.status(201).json("Home GET Request");
 })
+
+
+/** Start Server only when we have valid connection */
+connect().then(() => {
+    try{
+        app.listen(port, () => {
+            console.log(`Server Connected to http://localhost:${port}`);
+        })
+    } catch(error) {
+        console.log("cannot connect to the server");
+    }
+}).catch(error => {
+    console.log("Invalid Database Connection...!");
+})
+
+
